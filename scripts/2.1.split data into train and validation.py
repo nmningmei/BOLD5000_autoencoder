345f0fe29c4e5f69dc26f6c048ce557341881956
nmningmei <semsociaty@gmail.com>
2021-11-14 12:18:18 +0100

    update

--- scripts/2.1.split data into train and validation.py
+++ scripts/2.1.split data into train and validation.py
@@ -7,19 +7,23 @@ Created on Mon Aug 26 13:25:01 2019
 """
 
 import os
+import numpy as np
 from glob import glob
 from tqdm import tqdm
 from shutil import copyfile
 from sklearn.model_selection import train_test_split
 
-data_dir = '../data/volume_of_interest/'
+data_dir = '../../BOLD5000_data_store/data/volume_of_interest/'
 train_dir = '../data/train/'
 validation_dir = '../data/validation/'
 for d in [train_dir,validation_dir]:
     if not os.path.exists(d):
-        os.mkdir(d)
+        os.makedirs(d)
 
+subsample_for_test = int(1e4)
 all_files = glob(os.path.join(data_dir,"*.nii.gz"))
+if type(subsample_for_test) == int:
+    all_files = np.random.choice(all_files,size = subsample_for_test,replace = True,)
 
 train,test = train_test_split(all_files,test_size = 0.2,random_state = 12345)
 
